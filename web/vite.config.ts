31c9f36d092a0265f0d812a88446521e175bb366
Maksim Andriushin <m.andryushin@it-only.ru>
2024-12-02 01:49:36 +0400

    fixes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/',
+  plugins: [react(), tsconfigPaths(), svgr()],
+  server: {
+    port: 4000,
+    open: true,
+    watch: {
+      ignored: ['**/node_modules/**'],
+    },
+  },
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+});
