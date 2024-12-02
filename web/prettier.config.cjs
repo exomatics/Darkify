ec78e89037cf572fd56278139edbac7aab42143c
Maksim Andriushin <m.andryushin@it-only.ru>
2024-12-02 18:09:35 +0400

    fix frontend prettier

--- web/prettier.config.cjs
+++ web/prettier.config.cjs
@@ -8,11 +8,8 @@ module.exports = {
   jsxSingleQuote: false,
   trailingComma: 'all',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
   arrowParens: 'always',
   endOfLine: 'lf',
   rangeStart: 0,
   rangeEnd: Infinity,
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
 };
