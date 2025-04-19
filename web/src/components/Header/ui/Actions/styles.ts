676cfbeb329dee93996d803e98145d530cc8c6f7
Maxim <67149366+makandrr@users.noreply.github.com>
2025-04-19 15:55:28 +0400

    feat (#28)
    Co-authored-by: Maxim <maksim.andriushin@gmail.com>

---
+++ web/src/components/Header/ui/Actions/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledDropdown = styled.div`
+  background: rgba(32, 32, 32, 0.8);
+  backdrop-filter: blur(10px);
+  border-radius: 5px;
+  padding: 5px;
+`;
