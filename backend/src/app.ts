1bdc8d57f00be9f30761b7a02affae5d342666a1
Maxim <maksim.andriushin@gmail.com>
2025-05-01 00:08:07 +0400

    add swagger and serve api file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import fs from 'node:fs';
 
 import express from 'express';
 import passport from 'passport';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 import passportConfiguration from './config/authentication.ts';
-import { STATIC_DIRECTORY_PATH, PATH_TO_IMAGES } from './config/config.ts';
+import { STATIC_DIRECTORY_PATH, PATH_TO_IMAGES, PATH_TO_OPENAPI } from './config/config.ts';
 import logger from './config/logger.ts';
 import errorHandler from './middleware/error-handler.ts';
 import { jwtProcess } from './middleware/jwt-processing.ts';
@@ -16,10 +18,14 @@ if (!fs.existsSync(PATH_TO_IMAGES)) {
   fs.mkdirSync(PATH_TO_IMAGES, { recursive: true });
 }
 
+const openapiDocument = YAML.load(PATH_TO_OPENAPI);
+
 const PORT = process.env.PORT;
 const app = express();
 app.disable('x-powered-by');
 app.use(express.json());
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(openapiDocument));
+app.use('/docs', express.static(PATH_TO_OPENAPI));
 app.use(STATIC_DIRECTORY_PATH, express.static(PATH_TO_IMAGES));
 passportConfiguration(passport);
 app.use(passport.initialize());
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 
 const __dirname = import.meta.dirname;
-const PROJECT_ROOT = path.join(__dirname, '..', '..');
+export const PROJECT_ROOT = path.join(__dirname, '..', '..');
 export const PUBLIC_KEY_FILE_NAME = 'id_rsa_pub';
 export const PRIVATE_KEY_FILE_NAME = 'id_rsa_priv';
 export const ACCESS_TOKEN_EXPIRE_DATE = '30d';
@@ -10,3 +10,4 @@ export const DEFAULT_OFFSET = 0;
 export const DEFAULT_LIMIT = 20;
 export const PATH_TO_IMAGES = path.join(PROJECT_ROOT, 'uploads', 'images');
 export const STATIC_DIRECTORY_PATH = '/files';
+export const PATH_TO_OPENAPI = path.resolve(PROJECT_ROOT, 'docs', 'openapi.yaml');
