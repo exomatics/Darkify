01a4bf84f6c8b409b4a160bc26e1ec28d734b4d9
Maxim <maksim.andriushin@gmail.com>
2025-05-18 21:41:51 +0400

    fix prettier

--- web/src/api/gen/models/RegisterRequest.ts
+++ web/src/api/gen/models/RegisterRequest.ts
@@ -1,7 +1,7 @@
 /* generated using openapi-typescript-codegen -- do not edit */
 /* istanbul ignore file */
 /* tslint:disable */
- 
+
 import type { Password } from './Password';
 export type RegisterRequest = {
   email: string;
