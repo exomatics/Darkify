de14140b5b3507b6aefac8dba2129079939194cb
Maxim <maximdevtool@gmail.com>
2026-03-23 21:35:20 +0400

    fixes

--- backend/src/models/services/dashboard.ts
+++ backend/src/models/services/dashboard.ts
@@ -4,7 +4,6 @@ import { STATIC_IMAGES_PATH } from '../../config/config.ts';
 import database from '../../config/database.ts';
 import { Restrictions, Type } from '../../interfaces/playlist-interface.ts';
 
-import type { PlaylistAlbumInstanceWithRelations } from '../../interfaces/album-interface.ts';
 import type {
   RandomAlbum,
   RandomArtist,
@@ -20,44 +19,49 @@ type PlaylistInstanceWithUser = PlaylistModel & {
   user: UserModel;
 };
 
+type UserModelWithArtist = UserModel & {
+  artist?: { banner_id: string | null };
+};
+
 class DashboardManager {
-  async getRecentReleases(): Promise<SuccessfulResult<RecentRelease[]>> {
+  async getRecentReleases(userId: string): Promise<SuccessfulResult<RecentRelease[]>> {
     // eslint-disable-next-line sonarjs/sql-queries
     const recentReleases = (await database.sequelize.query(
       `
             SELECT *
             FROM (
-              SELECT 
+              SELECT
                 tracks.id,
                 tracks.name,
                 'single' AS type,
                 (library_releases.id IS NOT NULL) AS is_followed,
                 creation_date AS date_released,
-                CASE 
-                    WHEN tracks.cover_id IS NOT NULL 
+                CASE
+                    WHEN tracks.cover_id IS NOT NULL
                     THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', tracks.cover_id, '.jpg')
                     ELSE NULL
                 END AS cover_url
               FROM "tracks"
-              LEFT JOIN library_releases on tracks.id = library_releases.track_id 
+              LEFT JOIN library_releases on tracks.id = library_releases.track_id AND library_releases.user_id = ${database.sequelize.escape(userId)}
 
               UNION ALL
-          
-              SELECT 
+
+              SELECT
                 playlists.id,
                 playlists.name,
                 'album' AS type,
                 (library_releases.id IS NOT NULL) AS is_followed,
                 playlist_albums.date_released AS date_released,
-                CASE 
-                    WHEN playlists.cover_id IS NOT NULL 
+                CASE
+                    WHEN playlists.cover_id IS NOT NULL
                     THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', playlists.cover_id, '.jpg')
                     ELSE NULL
                 END AS cover_url
               FROM "playlists"
               INNER JOIN playlist_albums on playlists.id = playlist_albums.playlist_id
-              LEFT JOIN library_releases on playlists.id = library_releases.album_id WHERE playlists.type = ${database.sequelize.escape(Type.Album)} 
- 
+              LEFT JOIN library_releases on playlists.id = library_releases.album_id AND library_releases.user_id = ${database.sequelize.escape(userId)}
+              WHERE playlists.type = ${database.sequelize.escape(Type.Album)}
+
             ) AS releases
             ORDER BY "date_released" DESC
             LIMIT 20
@@ -66,46 +70,63 @@ class DashboardManager {
     )) as unknown as RecentRelease[];
     return { success: true, data: recentReleases };
   }
-  async getRandomAlbums(): Promise<SuccessfulResult<RandomAlbum[]>> {
-    const albumRecords = (await database.playlistModel.scope('albumOnly').findAll({
-      order: database.sequelize.random(),
-      include: [
-        {
-          model: database.playlistAlbumsModel,
-          attributes: ['date_released'],
-        },
-      ],
-      limit: 20,
-    })) as PlaylistAlbumInstanceWithRelations[];
-    const processedAlbumRecords = albumRecords.map((albumRecord) => {
-      return {
-        id: albumRecord.id,
-        name: albumRecord.name,
-        published: !!albumRecord.playlist_album.date_released,
-        date_released: albumRecord.playlist_album.date_released ?? null,
-        cover_url: albumRecord.cover_id
-          ? `${STATIC_IMAGES_PATH}/${albumRecord.cover_id}.jpg`
-          : null,
-      };
-    });
-    return { success: true, data: processedAlbumRecords };
+
+  async getRandomAlbums(userId: string): Promise<SuccessfulResult<RandomAlbum[]>> {
+    // eslint-disable-next-line sonarjs/sql-queries
+    const albumRecords = (await database.sequelize.query(
+      `
+        SELECT
+          p.id,
+          p.name,
+          pa.date_released,
+          COUNT(pt.track_id) AS count,
+          (lr.id IS NOT NULL) AS is_followed,
+          CASE
+            WHEN p.cover_id IS NOT NULL
+            THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', p.cover_id, '.jpg')
+            ELSE NULL
+          END AS cover_url
+        FROM playlists p
+        INNER JOIN playlist_albums pa ON p.id = pa.playlist_id
+        LEFT JOIN playlist_tracks pt ON p.id = pt.playlist_id
+        LEFT JOIN library_releases lr ON p.id = lr.album_id AND lr.user_id = ${database.sequelize.escape(userId)}
+        WHERE p.type = ${database.sequelize.escape(Type.Album)} AND pa.date_released IS NOT NULL
+        GROUP BY p.id, p.name, pa.date_released, p.cover_id, lr.id
+        ORDER BY RANDOM()
+        LIMIT 20
+      `,
+      { type: QueryTypes.SELECT },
+    )) as unknown as RandomAlbum[];
+    return { success: true, data: albumRecords };
   }
+
   async getRandomArtists(): Promise<SuccessfulResult<RandomArtist[]>> {
-    const artistRecords = await database.userModel.findAll({
+    const artistRecords = (await database.userModel.findAll({
       order: database.sequelize.random(),
       where: { is_artist: true },
       attributes: ['id', 'avatar_url', 'visible_username'],
+      include: [
+        {
+          model: database.artistModel,
+          attributes: ['banner_id'],
+          required: false,
+        },
+      ],
       limit: 20,
-    });
+    })) as unknown as UserModelWithArtist[];
     const processedArtistRecords = artistRecords.map((row) => {
       return {
         id: row.id,
-        visible_username: row.visible_username,
-        cover_url: row.avatar_url ? `${STATIC_IMAGES_PATH}/${row.avatar_url}.jpg` : null,
+        name: row.visible_username,
+        avatar_url: row.avatar_url ? `${STATIC_IMAGES_PATH}/${row.avatar_url}.jpg` : null,
+        banner_url: row.artist?.banner_id
+          ? `${STATIC_IMAGES_PATH}/${row.artist.banner_id}.jpg`
+          : null,
       };
     });
     return { success: true, data: processedArtistRecords };
   }
+
   async getRandomPlaylists(): Promise<SuccessfulResult<RandomPlaylist[]>> {
     const playlistRecords = (await database.playlistModel.findAll({
       where: { restrictions: Restrictions.Public },
@@ -126,51 +147,52 @@ class DashboardManager {
     });
     return { success: true, data: processedAlbumRecords };
   }
+
   async getRecentlyPlayed(userId: string): Promise<SuccessfulResult<RecentlyPlayed[]>> {
     // eslint-disable-next-line sonarjs/sql-queries
     const recentlyPlayed = (await database.sequelize.query(
       `
-              SELECT *
-              FROM (
-                SELECT 
-                  tracks.id,
-                  tracks.name,
-                  'single' AS type,
-                  (library_releases.id IS NOT NULL) AS is_followed,
-                  creation_date AS date_released,
-                  library_releases.date_played AS date_played,
-                  CASE 
-                      WHEN tracks.cover_id IS NOT NULL 
-                      THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', tracks.cover_id, '.jpg')
-                      ELSE NULL
-                  END AS cover_url
-                FROM "tracks"
-                INNER JOIN library_releases on tracks.id = library_releases.track_id 
-                WHERE library_releases.date_played IS NOT NULL AND library_releases.user_id = ${database.sequelize.escape(userId)}
-                
-                UNION ALL
-            
-                SELECT 
-                  playlists.id,
-                  playlists.name,
-                  'album' AS type,
-                  (library_releases.id IS NOT NULL) AS is_followed,
-                  playlist_albums.date_released AS date_released,
-                  library_releases.date_played AS date_played,
-                  CASE 
-                      WHEN playlists.cover_id IS NOT NULL 
-                      THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', playlists.cover_id, '.jpg')
-                      ELSE NULL
-                  END AS cover_url
-                FROM "playlists"
-                INNER JOIN playlist_albums on playlists.id = playlist_albums.playlist_id
-                INNER JOIN library_releases on playlists.id = library_releases.album_id 
-                WHERE playlists.type = ${database.sequelize.escape(Type.Album)}
-                AND library_releases.date_played IS NOT NULL AND library_releases.user_id = ${database.sequelize.escape(userId)}
-              ) AS releases
-              ORDER BY "date_played" DESC
-              LIMIT 9
-            `,
+        SELECT *
+        FROM (
+          SELECT
+            tracks.id,
+            tracks.name,
+            'single' AS type,
+            (library_releases.id IS NOT NULL) AS is_followed,
+            creation_date AS date_released,
+            library_releases.date_played AS date_played,
+            CASE
+                WHEN tracks.cover_id IS NOT NULL
+                THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', tracks.cover_id, '.jpg')
+                ELSE NULL
+            END AS cover_url
+          FROM "tracks"
+          INNER JOIN library_releases on tracks.id = library_releases.track_id
+          WHERE library_releases.date_played IS NOT NULL AND library_releases.user_id = ${database.sequelize.escape(userId)}
+
+          UNION ALL
+
+          SELECT
+            playlists.id,
+            playlists.name,
+            'album' AS type,
+            (library_releases.id IS NOT NULL) AS is_followed,
+            playlist_albums.date_released AS date_released,
+            library_releases.date_played AS date_played,
+            CASE
+                WHEN playlists.cover_id IS NOT NULL
+                THEN CONCAT(${database.sequelize.escape(STATIC_IMAGES_PATH)}, '/', playlists.cover_id, '.jpg')
+                ELSE NULL
+            END AS cover_url
+          FROM "playlists"
+          INNER JOIN playlist_albums on playlists.id = playlist_albums.playlist_id
+          INNER JOIN library_releases on playlists.id = library_releases.album_id
+          WHERE playlists.type = ${database.sequelize.escape(Type.Album)}
+          AND library_releases.date_played IS NOT NULL AND library_releases.user_id = ${database.sequelize.escape(userId)}
+        ) AS releases
+        ORDER BY "date_played" DESC
+        LIMIT 9
+      `,
       { type: QueryTypes.SELECT },
     )) as unknown as RecentlyPlayed[];
     return { success: true, data: recentlyPlayed };
